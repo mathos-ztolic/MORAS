af7a475b268238014a6e4e986cbba748809b5967
Zlatko Tolić <ztolic@mathos.hr>
2023-12-12 07:22:35 +0100

    zd3 - zadatak 3b

---
+++ zadaca_3/zadatak_3/b.asm
@@ -0,0 +1,52 @@
+$SET(0,7)
+$SET(offset,100)
+
+$MV(0,iterations)
+@iterations
+M=M-1
+$SET(idx,0)
+
+
+
+$WHILE(iterations)
+	
+	$SUB(0,idx,inner_iterations)
+	@inner_iterations
+	M=M-1
+	
+	$MV(idx,inner_idx)
+	@inner_idx
+	M=M+1
+	
+	$SUM(idx,offset,position)
+	
+	$WHILE(inner_iterations)
+		$SUM(inner_idx,offset,inner_position)
+
+		@position
+		A=M
+		D=M
+		@inner_position
+		A=M
+		D=D-M
+		@dont_swap
+		D;JLE
+
+		$SWPPTR(inner_position,position)
+
+		(dont_swap)
+		@inner_idx
+		M=M+1
+		@inner_iterations
+		M=M-1
+	$END
+
+	@idx
+	M=M+1
+
+	@iterations
+	M=M-1
+
+$END
+
+$HALT()
