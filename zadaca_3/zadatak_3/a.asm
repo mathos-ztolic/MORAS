00796c013245375cc0363e6646ff8cd1eea1945f
Zlatko Tolić <ztolic@mathos.hr>
2023-12-12 05:52:01 +0100

    zd3 - zadatak 3a

---
+++ zadaca_3/zadatak_3/a.asm
@@ -0,0 +1,35 @@
+$SET(0,3) // base
+$SET(1,3) // exponent
+
+$SET(2,0) // result
+
+$MV(0,base)
+$MV(1,exponent)
+
+$SET(nextadd,1)
+
+$WHILE(exponent) // {
+
+	$WHILE(base) // {
+  	$SUM(2, nextadd, 2)
+  	@base
+  	M=M-1
+  $END
+  // }
+
+  $MV(2,nextadd)
+  $SET(2,0)
+
+  @exponent
+  M=M-1
+
+  $MV(0,base)
+
+$END
+// }
+
+$MV(nextadd,2)
+
+(END)
+@END
+0;JMP
